6650f1708dc43ce4a1e2ba33374f4ceb640973e7
Jonathan <48403308+mesielepush@users.noreply.github.com>
2019-10-31 10:56:19 -0600

    fixed

--- main.rb
+++ main.rb
@@ -10,13 +10,13 @@ puts '| ################################## |'
 puts ''
 getting_players = true
 while getting_players
-    n_players = gets
-    puts n_players
-    puts n_players == '2'
-    if n_players == '2'
+    n_players = gets.chomp
+    
+    case n_players
+    when '2'
       puts "| You have chosen #{n_players} "
       getting_players = false
-    elsif n_players == '1'
+    when '1'
       puts "| You have chosen #{n_players}, you'll get a friend someday don't worry "
       getting_players = false
     else
